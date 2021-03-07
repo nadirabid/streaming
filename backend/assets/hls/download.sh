9122fb7b5ce5d091569d0ba6fc17b7719ccb98e3
Nadir Muzaffar <nadirmuzaffar@Nadirs-MBP.attlocal.net>
2021-03-06 23:58:00 -0800

    fake gen

--- backend/assets/hls/download.sh
+++ backend/assets/hls/download.sh
@@ -1,31 +1,116 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 set -e # exit when any command fails
 
-rm -rf summer_adrift* # clean old video files
+vlod() {
+    # Usage create-vod-hls.sh SOURCE_FILE [OUTPUT_NAME]
+    [[ ! "${1}" ]] && echo "Usage: create-vod-hls.sh SOURCE_FILE [OUTPUT_NAME]" && exit 1
 
-brew install youtube-dl
+    # comment/add lines here to control which renditions would be created
+    renditions=(
+        # resolution  bitrate  audio-rate
+        #  "426x240    400k    64k"
+        #  "640x360    800k     96k"
+        "842x480    1400k    128k"
+        "1280x720   2800k    128k"
+        "1920x1080  5000k    192k"
+    )
 
-youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' -o summer_adrift1.mp4 'https://www.youtube.com/watch?v=W1yaBmL42tY'
-youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' -o summer_adrift2.mp4 'https://www.youtube.com/watch?v=SuEZ3dJ4jxc'
-youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' -o summer_adrift3.mp4 'https://www.youtube.com/watch?v=-j6eKmu6qLE'
+    segment_target_duration=4       # try to create a new segment every X seconds
+    max_bitrate_ratio=1.07          # maximum accepted bitrate fluctuations
+    rate_monitor_buffer_ratio=1.5   # maximum buffer size between bitrate conformance checks
 
-for i in 1 2 3
-do 
-    mkdir -p summer_adrift$i/thumbnails
+    #########################################################################
 
-    # cut to 30 seconds or hls will take time
-    ffmpeg -ss 00:00:00 -i summer_adrift$i.mp4 -ss 00:00:00 -t 00:00:30 summer_adrift$i/vid.mp4
+    source="${1}"
+    target="${2}"
+    if [[ ! "${target}" ]]; then
+    target="${source##*/}" # leave only last component of path
+    target="${target%.*}"  # strip extension
+    fi
+    mkdir -p ${target}
 
-    # create hls
-    sh vlod.sh summer_adrift$i/vid.mp4 summer_adrift$i/hls
 
-    # create thumbnails
-    for size in 842x480 1280x720 1920x1080
-    do
-        ffmpeg -i summer_adrift$i/vid.mp4 -vf  "thumbnail,scale=$size" -frames:v 1 summer_adrift$i/thumbnails/$size.jpg
+    key_frames_interval="$(echo `ffprobe ${source} 2>&1 | grep -oE '[[:digit:]]+(.[[:digit:]]+)? fps' | grep -oE '[[:digit:]]+(.[[:digit:]]+)?'`*2 | bc || echo '')"
+    key_frames_interval=${key_frames_interval:-50}
+    key_frames_interval=$(echo `printf "%.1f\n" $(bc -l <<<"$key_frames_interval/10")`*10 | bc) # round
+    key_frames_interval=${key_frames_interval%.*} # truncate to integer
+
+    # static parameters that are similar for all renditions
+    static_params="-c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0"
+    static_params+=" -g ${key_frames_interval} -keyint_min ${key_frames_interval} -hls_time ${segment_target_duration}"
+    static_params+=" -hls_playlist_type vod"
+
+    # misc params
+    misc_params="-hide_banner -y"
+
+    master_playlist="#EXTM3U
+    #EXT-X-VERSION:3
+    "
+    cmd=""
+    for rendition in "${renditions[@]}"; do
+    # drop extraneous spaces
+    rendition="${rendition/[[:space:]]+/ }"
+
+    # rendition fields
+    resolution="$(echo ${rendition} | cut -d ' ' -f 1)"
+    bitrate="$(echo ${rendition} | cut -d ' ' -f 2)"
+    audiorate="$(echo ${rendition} | cut -d ' ' -f 3)"
+
+    # calculated fields
+    width="$(echo ${resolution} | grep -oE '^[[:digit:]]+')"
+    height="$(echo ${resolution} | grep -oE '[[:digit:]]+$')"
+    maxrate="$(echo "`echo ${bitrate} | grep -oE '[[:digit:]]+'`*${max_bitrate_ratio}" | bc)"
+    bufsize="$(echo "`echo ${bitrate} | grep -oE '[[:digit:]]+'`*${rate_monitor_buffer_ratio}" | bc)"
+    bandwidth="$(echo ${bitrate} | grep -oE '[[:digit:]]+')000"
+    name="${height}p"
+    
+    cmd+=" ${static_params} -vf scale=w=${width}:h=${height}:force_original_aspect_ratio=decrease"
+    cmd+=" -b:v ${bitrate} -maxrate ${maxrate%.*}k -bufsize ${bufsize%.*}k -b:a ${audiorate}"
+    cmd+=" -hls_segment_filename ${target}/${name}_%03d.ts ${target}/${name}.m3u8"
+    
+    # add rendition entry in the master playlist
+    master_playlist+="#EXT-X-STREAM-INF:BANDWIDTH=${bandwidth},RESOLUTION=${resolution}\n${name}.m3u8\n"
     done
+
+    # start conversion
+    echo -e "Executing command:\nffmpeg ${misc_params} -i ${source} ${cmd}"
+    ffmpeg ${misc_params} -i ${source} ${cmd}
+
+    # create master playlist file
+    echo -e "${master_playlist}" > ${target}/playlist.m3u8
+
+    echo "Done - encoded HLS is at ${target}/"
+}
+
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+while getopts "n:u:" arg; do
+  case $arg in
+    n) NAME=$OPTARG;;
+    u) URL=$OPTARG;;
+  esac
 done
 
-rm -rf summer_adrift*.mp4
-rm -rf summer_adrift*/*.mp4
+rm -rf $BASE_DIR/$NAME* # clean old video files
+
+brew list youtube-dl || brew install youtube-dl
+
+youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best' -o $BASE_DIR/$NAME.mp4 $URL
+
+mkdir -p $BASE_DIR/$NAME/thumbnails
+
+# cut to 30 seconds or hls will take time
+ffmpeg -ss 00:00:00 -i $BASE_DIR/$NAME.mp4 -ss 00:00:00 -t 00:00:30 $BASE_DIR/$NAME/vid.mp4
+
+# create hls
+vlod $BASE_DIR/$NAME/vid.mp4 $BASE_DIR/$NAME/hls
+
+# create thumbnails
+for size in 842x480 1280x720 1920x1080
+do
+    ffmpeg -i $BASE_DIR/$NAME/vid.mp4 -vf  "thumbnail,scale=$size" -frames:v 1 $BASE_DIR/$NAME/thumbnails/$size.jpg
+done
+
+rm -rf $BASE_DIR/$NAME*.mp4
+rm -rf $BASE_DIR/$NAME*/*.mp4
--- backend/cmd/fake.go
+++ backend/cmd/fake.go
@@ -1,7 +1,8 @@
 package cmd
 
 import (
-	"streaming/models"
+	"fmt"
+	"os/exec"
 	"streaming/utils"
 
 	"github.com/spf13/cobra"
@@ -9,24 +10,39 @@ import (
 
 func fake(cmd *cobra.Command, args []string) error {
 	// init viper
-	confFile, err := cmd.Flags().GetString("conf")
-	v, err := utils.ConfigureViper(confFile)
-	if err != nil {
-		return err
-	}
+	// confFile, err := cmd.Flags().GetString("conf")
+	// v, err := utils.ConfigureViper(confFile)
+	// if err != nil {
+	// 	return err
+	// }
 
 	// setup db
 
-	db, err := models.NewDatabase(v)
-	if err != nil {
-		return err
+	// db, err := models.NewDatabase(v)
+	// if err != nil {
+	// 	return err
+	// }
+
+	// lets go: download & update db
+	urls := map[string]string{
+		"summer_adrif1": "https://www.youtube.com/watch?v=W1yaBmL42tY",
+		"summer_adrif2": "https://www.youtube.com/watch?v=SuEZ3dJ4jxc",
+		"summer_adrif3": "https://www.youtube.com/watch?v=-j6eKmu6qLE",
 	}
 
-	for i := 1; i <= 3; i++ {
-		video := &models.Video{
-			Name:        "Part 1",
-			Description: "Starting the journey",
+	downloadSh := utils.GetAbsolutePath("assets/hls/download.sh")
+
+	for name, url := range urls {
+		cmdStr := fmt.Sprintf("%s -n %s -u '%s'", downloadSh, name, url)
+		output, err := exec.Command("/bin/sh", cmdStr).Output()
+		warner.Println(cmdStr)
+
+		if err != nil {
+			errer.Println(output, err.Error())
+			return err
 		}
+
+		successer.Println(output)
 	}
 
 	return nil
